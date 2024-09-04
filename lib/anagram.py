343aa204eabbc24e1a0513a2b61a0d7318df8ef3
Philip <membaphilip1@gmail.com>
2024-09-04 14:20:58 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word.lower()
+        self.sorted_word=sorted(self.word)
+
+    def match(self,possible_anagrams):
+        matches=[]
+
+        for watu in possible_anagrams:
+            if sorted(watu.lower())==self.sorted_word:
+                matches.append(watu)
+
+        return matches
+    
